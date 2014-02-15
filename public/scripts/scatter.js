62c63da6d37806d23e733055738a91bda38d4de8
jgpettibone <jeanette.pettibone@gmail.com>
2014-02-15 15:03:18 -0800

    two lines that mimic algorithm learning models

--- public/scripts/scatter.js
+++ public/scripts/scatter.js
@@ -2,15 +2,10 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40}
 
 var width = 700 - margin.left - margin.right
 var height = 450 - margin.top - margin.bottom
-var radius = 2
+var radius = 3
 var padding = radius*4
 var fvectors = 100
 var classes = 4
-
-// var qw = width/2 - 50;
-// var qh = height/2 - 50;
-
-
 
 var svg = d3.select('.scatter')
   .append('svg')
@@ -21,6 +16,7 @@ var dataq1 = d3.range(fvectors/classes+10)
 var dataq2 = d3.range(fvectors/classes+1,fvectors/classes*2+10)
 var dataq3 = d3.range(fvectors/classes*2+11,fvectors/classes*4)
 // var dataq4 = d3.range(fvectors/classes)
+console.log(dataq1.concat(dataq2))
 
 svg.selectAll('circle')
   .data(dataq1)
@@ -41,13 +37,15 @@ svg.selectAll('circle')
   .enter().append('circle')
   .attr({'r': radius, 'class': 'fvector class3'})
   .attr('cx', function() { return Math.random()*(width-(width/2)-80) + width/2})
-  .attr('cy', function() { return Math.random()*height + 50})
+  .attr('cy', function() { return Math.random()*height + 60})
 
 var currentIndex = 0;
 var firstline = {x1: 0, y1: height/2-100, x2: width, y2: height/2+100}
 var secondline = {x1: width/3, y1: height, x2: (width/3)*2, y2: 0}
+var thirdline = {x1: 0, y1: (height/3)*2, x2: (width/5)*4, y2: 0}
+var fourthline = {x1: 0, y1: (height/3), x2: (width/2), y2: height}
 var finalline = {x1: width/5, y1: 0, x2: width/2, y2: height}
-var alllines = [firstline, secondline, finalline]
+var alllines = [secondline, firstline, thirdline, fourthline, finalline]
 
 svg.selectAll('line')
   .data([1])
@@ -60,10 +58,10 @@ svg.selectAll('line')
   .attr('stroke-dasharray', (20,20))
   .attr('stroke', 'black');
 
-currentIndex++;
 var timer = setInterval(function() {
+  currentIndex++;
   if (currentIndex === alllines.length) {
-    currentIndex = 0;
+    // currentIndex = 0;
     clearTimer();
   } else {  
     svg.selectAll('line')
@@ -72,13 +70,38 @@ var timer = setInterval(function() {
         .attr('y1', alllines[currentIndex].y1)
         .attr('x2', alllines[currentIndex].x2)
         .attr('y2', alllines[currentIndex].y2);
-
-      currentIndex++;
   } 
 }, 1000);
 
+var nextline = {x1: 0, y1: (height/3)*2, x2: (width/5)*4, y2: 0}
+var finalshortline = {x1: width/3-10, y1: (height/3)+30, x2: (width/5)*4, y2: 0}
+
 var clearTimer = function() {
   clearInterval(timer);
-}
+  svg.selectAll('line')
+    .attr('stroke-dasharray', (0,0))
+    .attr('stroke', 'red')
+    .data([1,2])
+    .enter().append('line')
+      .attr('class', 'change')
+      .attr('x1', nextline.x1)
+      .attr('y1', nextline.y1)
+      .attr('x2', nextline.x2)
+      .attr('y2', nextline.y2)
+      .attr('stroke-width', 4)
+      .attr('stroke-dasharray', (20,20))
+      .attr('stroke', 'black');
+  setTimeout(function() {
+    svg.select('.change')
+      .attr('x1', finalshortline.x1)
+      .attr('y1', finalshortline.y1)
+      .attr('x2', finalshortline.x2)
+      .attr('y2', finalshortline.y2)
+      .attr('stroke-width', 4)
+      .attr('stroke-dasharray', (0,0))
+      .attr('stroke', 'red');
+
+  },1000);
+};
 
 

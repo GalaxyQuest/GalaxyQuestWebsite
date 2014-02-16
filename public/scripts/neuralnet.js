927987e0fe5704d5f8bc1732453f2594aa405617
jgpettibone <jeanette.pettibone@gmail.com>
2014-02-15 16:32:12 -0800

    first nodes of the neural network

--- public/scripts/neuralnet.js
+++ public/scripts/neuralnet.js
@@ -2,12 +2,33 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40}
 
 var width = 700 - margin.left - margin.right
 var height = 450 - margin.top - margin.bottom
-var radius = 3
+var radius = 20
 var padding = radius*4
 // var fvectors = 100
 // var classes = 4
 
-var svg = d3.select('.scatter')
+var svg = d3.select('.neuralnet')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
+
+svg.selectAll('circle')
+  .data([1,2,3])
+  .enter().append('circle')
+  .attr({'r': radius, 'class': 'input'})
+  .attr('cx', function() { return 100;})
+  .attr('cy', function() { return d3.select(this).data()[0]*100;})
+
+svg.selectAll('circle')
+  .data([1,2,3,4,5,6,7])
+  .enter().append('circle')
+  .attr({'r': radius, 'class': 'input'})
+  .attr('cx', function() {return 250;})
+  .attr('cy', function() { return (d3.select(this).data()[0]-3.5)*100;})
+
+svg.selectAll('circle')
+  .data([1,2,3,4,5,6,7,8,9])
+  .enter().append('circle')
+  .attr({'r': radius, 'class': 'input'})
+  .attr('cx', function() {return 400;})
+  .attr('cy', function() { return (d3.select(this).data()[0]-6.5)*100;})

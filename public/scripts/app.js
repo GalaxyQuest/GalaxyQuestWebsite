c2256cbdaabc9cd1f1e2be4f07429fc977c8c798
jgpettibone <jeanette.pettibone@gmail.com>
2014-02-16 18:00:49 -0800

    angularizing and modularizing website

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,25 +3,43 @@ angular.module('galaxyApp', ['ngRoute'])
   .config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-      .when('/#', {
-        // templateUrl: '../views/main.html',
-        templateUrl: './index.html',
-        controller: 'MainController'
+      .when('/', {
+        templateUrl: '../views/mainindex.html',
+        controller: 'FrameController'
       })
-      .when('/neuralnet', {
-        templateUrl: '../views/neuralnet.html',
-        controller: 'NeuralNetController'
-      })
-      .when('/scatter', {
-        templateUrl: '../views/scatter.html',
+      // .when('/galaxyclass', {
+      //   templateUrl: '../views/partials/galaxyclass.html',
+      //   controller: 'GalaxyClassController'
+      // })
+      // .when('/contact', {
+      //   templateUrl: '../views/partials/contact.html',
+      //   controller: 'ContactController'
+      // })      
+      .when('/algorithms', {
+        templateUrl: '../views/partials/scatter.html',
         controller: 'ScatterController'
+      })      
+      // .when('/scatter', {
+      //   templateUrl: '../views/partials/scatter.html',
+      //   controller: 'ScatterController'
+      // })
+      .otherwise({
+        redirectTo: '/'
       });
   })
 
+  .controller('FrameController', function($scope) {
+  })
+  .controller('MainController', function($scope) {
+  })
+  .controller('ContactController', function($scope) {
+  })
+  .controller('GalaxyClassController', function($scope) {
+  })
+  .controller('AlgorithmsController', function($scope) {
+  })
   .controller('ScatterController', function($scope) {
-    console.log('scatter');
   })
   .controller('NeuralNetController', function($scope) {
-    console.log('hello');
   });
 
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,12 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Galaxy Quest' });
 };
+
+// exports.scatter = function(req, res) {
+//   // var name = req.params.name;
+//   // console.log('working');
+//   // console.log('rendering views/partials/' + name);
+//   // res.render('views/partials/' + name);
+//   res.render('../public/views/scatter');
+
+// }

7c41559d4c973dc5feda0c79d5b842092f608946
Scott Z <Scottzhang235@gmail.com>
2014-02-28 09:39:44 -0800

    updated guide page

--- public/scripts/controllers/guideController.js
+++ public/scripts/controllers/guideController.js
@@ -1,7 +1,7 @@
 galaxyApp.controller('GuideController', function($scope, $routeParams){
  var templates = {
-    'undefined' : '../../views/partials/mlintro.html',
-    'theBlueprint' : '../../views/partials/mlintro.html',
+    'undefined' : '../../views/partials/intro.html',
+    'ourOrigins' : '../../views/partials/intro.html',
     'discoveringOurData' : '../../views/partials/dataExplanation.html',
     'researchingOurPath' : '../../views/partials/randomForests.html',
     'testFlight' : '../../views/partials/iterationAndTesting.html',

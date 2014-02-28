6b35a78fadf75905f56383524c80f9ec421ef473
vingiarrusso <vin.giarrusso@gmail.com>
2014-02-28 15:25:20 -0800

    updated chart info

--- public/scripts/c3chart.js
+++ public/scripts/c3chart.js
@@ -2,13 +2,13 @@ var chart = c3.generate({
   bindto: '.errorChart',
   data: {
     columns: [
-      ['Root Means Squared Error', 0.16, 0.14, 0.1269, 0.1247]
+      ['Root Mean Squared Error', 0.16, 0.14, 0.1269, 0.1247, 0.1248, 0.1233]
     ]
   },
   axis: {
     x: {
       type: 'categorized',
-      categories: ['1st Iteration', '2nd Iteration', '3rd Iteration', '4th Iteration']
+      categories: ['1st Iteration', '2nd Iteration', '3rd Iteration', '4th Iteration', '5th Iteration', '6th Iteration']
     }
   }
 });

ab4dea16db2bd5a9bdfefbd9436c1c9f2d6dc4b4
Scott Z <Scottzhang235@gmail.com>
2014-02-28 15:37:24 -0800

    content to guide adjusted

--- public/scripts/controllers/guideController.js
+++ public/scripts/controllers/guideController.js
@@ -1,7 +1,7 @@
 galaxyApp.controller('GuideController', function($scope, $routeParams){
  var templates = {
     'undefined' : '../../views/partials/intro.html',
-    'ourOrigins' : '../../views/partials/intro.html',
+    'gettingStarted' : '../../views/partials/intro.html',
     'discoveringOurData' : '../../views/partials/dataExplanation.html',
     'plottingOurCourse' : '../../views/partials/randomForests.html',
     'testFlight' : '../../views/partials/iterationAndTesting.html',
